e92b38153a3ef5315f515f7e38341d34a54f1b47
Le Gia Thuan <legiathuan9a10@gmail.com>
2025-09-07 15:50:48 +0700

    use Css and variable of js in html block

--- src/component/learn/MyComponent.jsx
+++ src/component/learn/MyComponent.jsx
@@ -1,6 +1,19 @@
+// JSX: 1 parent
+//fragment <> </>
+// {} giup co the viet js trong khoi html
+
+import "./style.css";
+
 const MyComponent = () => {
+    const age = 21;
+    const obj = { name: 'AAA', email: 'AAA@gmail.com' };
     return (
-        <div>From: Le Gia Thuan Update</div>
+        // fragment giup phan tu boc ngoai khong render tag html ben ngoai
+        <>
+            <div>From: Le Gia Thuan</div>
+            <div>Age: {JSON.stringify(obj)}</div>
+            <div className="child">CSS Here</div>
+        </>
     );
 }
 

d16e7ced9c32b73b9f8b86edaf91e871450ca5d1
Le Gia Thuan <legiathuan9a10@gmail.com>
2025-09-14 20:30:15 +0700

    router with Link component(use Link for no reload page when change page)

--- src/component/layout/header.jsx
+++ src/component/layout/header.jsx
@@ -1,13 +1,14 @@
+import { Link } from "react-router-dom";
 import "./header.css"
 
 const Header = () => {
     return (
         <div className="navbar">
-            <ul className="nav-list">
-                <li className="list-item"><a href="/">Home</a></li>
-                <li className="list-item"><a href="/products">Product</a></li>
-                <li className="list-item"><a href="/users">Users</a></li>
-            </ul>
+            <div className="nav-list">
+                <Link  className="list-item active" to="/">Home</Link>
+                <Link className="list-item" to="/products">Product</Link>
+                <Link className="list-item" to="/users">Users</Link>
+            </div>
             <div className="login-register">
                 <a href="/login" className="btn">Login</a>
                 <a href="/register" className="btn">Register</a>

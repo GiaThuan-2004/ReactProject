09d0a37af9af84e45c9cff7918c0057a294a7442
Le Gia Thuan <legiathuan9a10@gmail.com>
2025-09-18 15:27:25 +0700

    add useState for user-form

--- src/component/user/user.form.jsx
+++ src/component/user/user.form.jsx
@@ -1,26 +1,52 @@
 import { Input, Button } from 'antd';
+import { useState } from "react";
 
 const UserForm = () => {
+    const [nameState, setNameState] = useState("");
+    const [emailState, setEmailState] = useState("");
+    const [passState, setPassState] = useState("");
+    const [phoneState, setPhoneState] = useState("");
+
+    const getInput = () => {
+        console.log(nameState, emailState, passState, phoneState);
+    }
+
     return (
         <div className="form-container">
             <div className='form-item'>
                 <span>FullName</span>
-                <Input></Input>
+                <Input
+                    value={nameState}
+                    onChange={event => setNameState(event.target.value)}>
+
+                </Input>
             </div>
             <div className='form-item'>
                 <span>Email</span>
-                <Input></Input>
+                <Input
+                    value={emailState}
+                    onChange={event => setEmailState(event.target.value)}>
+
+                </Input>
             </div>
             <div className='form-item'>
                 <span>Password</span>
-                <Input.Password></Input.Password>
+                <Input.Password
+                    value={passState}
+                    onChange={event => setPassState(event.target.value)}>
+
+                </Input.Password>
             </div>
             <div className='form-item'>
                 <span>Phone</span>
-                <Input></Input>
+                <Input
+                    value={phoneState}
+                    onChange={event => setPhoneState(event.target.value)}>
+
+                </Input>
             </div>
             <div>
-                <Button type='primary'>Create User</Button>
+                <Button type='primary' onClick={() => getInput()}>Create User</Button>
             </div>
         </div>
     );
